4e10db1c4c599a4481de5269f0d67cada568270c
Dmitry <khmylev@mail.ru>
2023-07-25 10:19:29 +0700

    25/07/2023

--- src/main/java/searchengine/Search.java
+++ src/main/java/searchengine/Search.java
@@ -1,22 +1,23 @@
 package searchengine;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
 import searchengine.DTO.MorphologyAnalyzerRequestDTO;
-import searchengine.models.*;
+import searchengine.DTO.SearchDataDTO;
+import searchengine.models.Indexing;
+import searchengine.models.Lemma;
+import searchengine.models.Page;
+import searchengine.models.Site;
 import searchengine.morphology.MorphologyAnalyzer;
 import searchengine.service.IndexRepositoryService;
 import searchengine.service.LemmaRepositoryService;
 import searchengine.service.PageRepositoryService;
 import searchengine.service.SiteRepositoryService;
 import searchengine.service.responses.SearchApiResponse;
-import searchengine.DTO.SearchDataDTO;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 @Service
@@ -37,11 +38,11 @@ public class Search {
         this.lemmaRepositoryService = lemmaRepositoryService;
     }
 
-    public SearchApiResponse searchService (MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO, String url, int offset, int limit) {
+    public SearchApiResponse searchService(MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO, String url, int offset, int limit) {
         List<Site> siteList = siteRepositoryService.getAllSites();
         List<SearchDataDTO> listOfSearchData = new ArrayList<>();
-        if(url == null) {
-            for(Site s : siteList){
+        if (url == null) {
+            for (Site s : siteList) {
                 Map<Page, Double> list = searching(morphologyAnalyzerRequestDTO, s.getId());
 
                 listOfSearchData.addAll(getSortedSearchData(list, morphologyAnalyzerRequestDTO));
@@ -53,10 +54,10 @@ public class Search {
         }
         int count;
         listOfSearchData.sort(Comparator.comparingDouble(SearchDataDTO::getRelevance));
-        if (listOfSearchData.isEmpty()){
+        if (listOfSearchData.isEmpty()) {
             return new SearchApiResponse(false);
         }
-        if(limit + offset < listOfSearchData.size()) {
+        if (limit + offset < listOfSearchData.size()) {
             count = limit;
         } else {
             count = listOfSearchData.size() - offset;
@@ -106,7 +107,7 @@ public class Search {
         return pageRelevance;
     }
 
-    private List<SearchDataDTO> getSortedSearchData (Map<Page, Double> sortedPageMap, MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO) {
+    private List<SearchDataDTO> getSortedSearchData(Map<Page, Double> sortedPageMap, MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO) {
         List<SearchDataDTO> responses = new ArrayList<>();
         LinkedHashMap<Page, Double> sortedByRankPages = (LinkedHashMap<Page, Double>) sortMapByValue(sortedPageMap);
         for (Map.Entry<Page, Double> page : sortedByRankPages.entrySet()) {
@@ -116,13 +117,13 @@ public class Search {
         return responses;
     }
 
-    private List<Lemma> sortedReqLemmas(MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO, int siteId){
+    private List<Lemma> sortedReqLemmas(MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO, int siteId) {
         List<Lemma> lemmaList = new ArrayList<>();
         List<String> list = morphologyAnalyzerRequestDTO.getReqLemmas();
-        for(String s : list) {
+        for (String s : list) {
             List<Lemma> reqLemmas = lemmaRepositoryService.getLemma(s);
-            for(Lemma l : reqLemmas){
-                if(l.getSiteId() == siteId){
+            for (Lemma l : reqLemmas) {
+                if (l.getSiteId() == siteId) {
                     lemmaList.add(l);
                 }
             }
@@ -131,7 +132,7 @@ public class Search {
         return lemmaList;
     }
 
-    private double getAbsRelevance(Page page, List<Lemma> lemmas){
+    private double getAbsRelevance(Page page, List<Lemma> lemmas) {
         double r = 0.0;
         int pageId = page.getId();
         for (Lemma lemma : lemmas) {
@@ -143,7 +144,7 @@ public class Search {
     }
 
 
-    private SearchDataDTO getResponseByPage (Page page, MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO, double relevance) {
+    private SearchDataDTO getResponseByPage(Page page, MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO, double relevance) {
         SearchDataDTO response = new SearchDataDTO();
         Site site = siteRepositoryService.getSite(page.getSiteId());
         String siteUrl = site.getUrl();
@@ -160,19 +161,19 @@ public class Search {
         return response;
     }
 
-    private String getTitle (String html){
+    private String getTitle(String html) {
         String string = "";
         Document document = Jsoup.parse(html);
         Elements elements = document.select("title");
         StringBuilder builder = new StringBuilder();
         elements.forEach(element -> builder.append(element.text()).append(" "));
-        if (!builder.isEmpty()){
+        if (!builder.isEmpty()) {
             string = builder.toString();
         }
         return string;
     }
 
-    private String getSnippet (String html, MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO) {
+    private String getSnippet(String html, MorphologyAnalyzerRequestDTO morphologyAnalyzerRequestDTO) {
         MorphologyAnalyzer analyzer = new MorphologyAnalyzer();
         String string = "";
         Document document = Jsoup.parse(html);
@@ -181,156 +182,79 @@ public class Search {
         StringBuilder builder = new StringBuilder();
         titleElements.forEach(element -> builder.append(element.text()).append(" ").append("\n"));
         bodyElements.forEach(element -> builder.append(element.text()).append(" "));
-        if (!builder.isEmpty()){
+        if (!builder.isEmpty()) {
             string = builder.toString();
         }
         List<String> req = morphologyAnalyzerRequestDTO.getReqLemmas();
         Map<String, Integer> integerList1 = new HashMap<>();
- //       Set<Integer> integerList = new TreeSet<>();
+
         for (String s : req) {
             integerList1.put(s, analyzer.findLemmaIndexInText(string, s).get(0));
- //           integerList.addAll(analyzer.findLemmaIndexInText(string, s));
         }
+        LinkedHashMap<String, Integer> sortedLinkedLemmasIndexes = sortLinkedMapByValue(integerList1);
 
-//        for (int key : integerList) {
-//
-//            int lengthShorts = 0;
-//            while (lengthShorts > 0 && !Character.isWhitespace(string.charAt(lengthShorts))) {
-//                lengthShorts++;
-//            }
-//
-//            String part1 = string.substring(0, key);
-//            String part2 = string.substring(key, lengthShorts);
-//            String part3 = string.substring(lengthShorts);
-//
-//            string = part1 + "<b>" + part2 + "</b>" + part3;
-//
-//        }
-
-
-
-        //       List<TreeSet<Integer>> indexesList = getSearchingIndexes(string, integerList);
         StringBuilder builder1 = new StringBuilder();
-//        for (TreeSet<Integer> set : indexesList) {
-//            int from = set.first();
-//            int to = set.last();
-//            Pattern pattern = Pattern.compile("\\p{Punct}|\\s");
-//            Matcher matcher = pattern.matcher(string.substring(to));
-//            int offset = 0;
-//            if (matcher.find()){
-//                offset = matcher.end();
-//            }
-//            builder1.append("<b>")
-//                    .append(string, from, to + offset)
-//                    .append("</b>");
-//            if (!((string.length() - to) < 40)) {
-//                builder1.append(string, to + offset, string.indexOf(" ", to + offset + 100))
-//                        .append("... ");
-//            }
-//        }
 
+        Iterator<Map.Entry<String, Integer>> iterator = sortedLinkedLemmasIndexes.entrySet().iterator();
+        Map.Entry<String, Integer> prevEntry = iterator.next();
+        int shorts = integerList1.size();
 
- //       int totalLength = 0;
- //       boolean ellipsisAdded = false;
+        while (iterator.hasNext()) {
+            Map.Entry<String, Integer> currentEntry = iterator.next();
+            int currentValue = currentEntry.getValue();
+            int prevValue = prevEntry.getValue();
+            int difference = currentValue - prevValue;
 
-
-
-//        String snippet = "";
-//        int reqQuantity = req.size();
-//        for (TreeSet<Integer> set : indexesList) {
-//            int from = set.first();
-//            int to = set.last();
-//            Pattern pattern = Pattern.compile("\\p{Punct}|\\s");
-//            Matcher matcher = pattern.matcher(string.substring(to));
-//            int offset = 0;
-//            if (matcher.find()) {
-//                offset = matcher.end();
-//            }
-//
-//            if (indexesList.size() != 1 && reqQuantity != 1) {
-//                snippet = "<b>" + string.substring(from, to + offset) + "</b>" + string.substring(to + offset);
-//                if (snippet.length() > 130) {
-//                    snippet = snippet.substring (0, 127) + " ... ";
-//                } else { snippet = snippet + "..."; }
-//            }
-//            else {
-//
-//                snippet = "<b>" + string.substring(from, to + offset) + "</b>" + string.substring(to + offset + 1, to + 250) + " ... ";
-//            }
-//                builder1.append(snippet);
-//        }
-
-
-            for (String val : integerList1.keySet()) {
-                int index = integerList1.get(val);
-                int shorts = integerList1.size();
-                int to = index + val.length();
-            int lengthShorts = to + 240 / shorts;
-            while (lengthShorts > 0 && !Character.isWhitespace(string.charAt(lengthShorts))) {
-                lengthShorts--;
+            int endIndex = string.indexOf(" ", prevValue + 1);
+            if (endIndex == -1) {
+                endIndex = string.length();
             }
 
-//            String snippet = string.substring(key, lengthShorts);
-            String snippet = "<b>" + string.substring(index, to) + "</b>" + string.substring(to, lengthShorts);
-//            if (to < string.length()) {
-//                snippet += string.substring(to, to + 1) + " ... ";
-//            } else {
-                snippet += "...";
- //           }
+            int to = endIndex;
+            if (difference < 240 / shorts) {
+                String snippet = "<b>" + string.substring(prevValue, to) + "</b>" + string.substring(to, currentValue - 1) + " ";
+                builder1.append(snippet);
+            } else {
+                String snippet = "<b>" + string.substring(prevValue, to) + "</b>" + string.substring(to, to + 240 / shorts);
+                snippet += "... ";
+                builder1.append(snippet);
+            }
+            prevEntry = currentEntry;
 
-            builder1.append(snippet);
         }
+        int endIndex = string.indexOf(" ", prevEntry.getValue() + 1);
+        if (endIndex == -1) {
+            endIndex = string.length();
+        }
+        int to = endIndex;
+        int endShort = 240 - builder1.length() - (to - prevEntry.getValue()) - 3;
+        String snippet = "<b>" + string.substring(prevEntry.getValue(), to) + "</b>" + string.substring(to, to + endShort);
+        snippet += "... ";
+
+        builder1.append(snippet);
         string = builder1.toString();
-        for (String val : integerList1.keySet()) {
-            string = string.replaceAll(val,"<b>" + val + "</b>" );
-
-        }
-        string = string.replaceAll("<b>" + "<b>","<b>");
-        string = string.replaceAll("</b>" + "</b>","</b>" );
 
         return string;
     }
 
 
-    private List<TreeSet<Integer>> getSearchingIndexes (String string, Set<Integer> indexesOfBolt) {
-        ArrayList<Integer> indexes = new ArrayList<>(indexesOfBolt);
-        List<TreeSet<Integer>> list = new ArrayList<>();
-        TreeSet<Integer> temp = new TreeSet<>();
-        for (int i = 0; i < indexes.size(); i++) {
-            String s = string.substring(indexes.get(i));
-            int end = s.indexOf(" ");
-            if ((i + 1) <= indexes.size() - 1 && (indexes.get(i + 1) - indexes.get(i)) < end + 5){
-                temp.add(indexes.get(i));
-                temp.add(indexes.get(i + 1));
-            }
-             else {
-                if (!temp.isEmpty()) {
-                    list.add(temp);
-                    temp = new TreeSet<>();
-                }
-                temp.add(indexes.get(i));
-                list.add(temp);
-                temp = new TreeSet<>();
-            }
-        }
-        list.sort((Comparator<Set<Integer>>) (o1, o2) -> o2.size() - o1.size());
-        ArrayList<TreeSet<Integer>> searchingIndexes = new ArrayList<>();
-        for (int i = 0; i < 2; i++) {
-            if(list.size() > i) {
-                searchingIndexes.add(list.get(i));
-            }
-        }
-        return searchingIndexes;
-    }
-
-    public  <K, V extends Comparable<? super V>> Map<K, V>
-    sortMapByValue(Map<K, V> map )
-    {
-        Map<K,V> result = new LinkedHashMap<>();
-        Stream<Map.Entry<K,V>> st = map.entrySet().stream();
+    public <K, V extends Comparable<? super V>> Map<K, V>
+    sortMapByValue(Map<K, V> map) {
+        Map<K, V> result = new LinkedHashMap<>();
+        Stream<Map.Entry<K, V>> st = map.entrySet().stream();
 
         st.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                .forEach(e ->result.put(e.getKey(),e.getValue()));
+                .forEach(e -> result.put(e.getKey(), e.getValue()));
+
+        return result;
+    }
+
+    public static <K, V extends Comparable<? super V>> LinkedHashMap<K, V> sortLinkedMapByValue(Map<K, V> map) {
+        LinkedHashMap<K, V> result = new LinkedHashMap<>();
+        Stream<Map.Entry<K, V>> st = map.entrySet().stream();
+
+        st.sorted(Map.Entry.comparingByValue())
+                .forEach(e -> result.put(e.getKey(), e.getValue()));
 
         return result;
     }
